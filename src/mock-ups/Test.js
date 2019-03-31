15686c03cd0a88efb2cea65f4c19aad2c974b5f8
mininerej <ejhong.4991@gmail.com>
2019-03-31 23:23:08 +0900

    build article page - scroll to the sentence which contains clicked word - resposive layout

---
+++ src/mock-ups/Test.js
@@ -0,0 +1,34 @@
+export default [
+  {
+    word: "book",
+    translation1: "[명사] 사과",
+    translation2: "[명사] 바나나",
+    translation3: "[명사] 책, 도서",
+    translation4: "[명사] 파인애플",
+    answer: 3
+  },
+  {
+    word: "apple",
+    translation1: "[명사] 사과",
+    translation2: "[명사] 바나나",
+    translation3: "[명사] 책, 도서",
+    translation4: "[명사] 파인애플",
+    answer: 1
+  },
+  {
+    word: "banana",
+    translation1: "[명사] 사과",
+    translation2: "[명사] 바나나",
+    translation3: "[명사] 책, 도서",
+    translation4: "[명사] 파인애플",
+    answer: 2
+  },
+  {
+    word: "pineapple",
+    translation1: "[명사] 사과",
+    translation2: "[명사] 바나나",
+    translation3: "[명사] 책, 도서",
+    translation4: "[명사] 파인애플",
+    answer: 4
+  }
+];

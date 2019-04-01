b74aadb53b743e133cb35cf42b20fd21cca4494d
mininerej <ejhong.4991@gmail.com>
2019-04-01 12:37:12 +0900

    fix news routing problem - remove <Router> tag from Newses.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
 import Newses from "./Newses";
 import Wordbook from "./Wordbook";
 import Test from "./Test";
@@ -13,47 +13,47 @@ export default class Header extends Component {
   render() {
     return (
       <div className="header">
-        <Router>
-          <nav className="nav">
-            <div className="nav-menu">
-              <Link to={"/"}>
-                <img alt="logo" src={logo} className="nav-logo" />
-              </Link>
-              <ul className="nav-ul">
-                <li className="nav-item">
-                  <Link to={"/"}>News</Link>
-                </li>
-                <li className="nav-item">
-                  <Link to={"/words"}>Wordbook</Link>
-                </li>
-                <li className="nav-item">
-                  <Link to={"/test"}>Test</Link>
-                </li>
-                <li className="nav-item">
-                  <Link to={"/setting"}>Setting</Link>
-                </li>
-                <li className="nav-item">
-                  <a onClick={() => {}}>Sign In</a>
-                </li>
-                <li className="nav-item">
-                  <a onClick={() => {}}>Sign Up</a>
-                </li>
-              </ul>
-            </div>
+        <nav className="nav">
+          <div className="nav-menu">
+            <Link to={"/"}>
+              <img alt="logo" src={logo} className="nav-logo" />
+            </Link>
+            <ul className="nav-ul">
+              <li className="nav-item">
+                <Link to={"/"}>News</Link>
+              </li>
+              <li className="nav-item">
+                <Link to={"/words"}>Wordbook</Link>
+              </li>
+              <li className="nav-item">
+                <Link to={"/test"}>Test</Link>
+              </li>
+              <li className="nav-item">
+                <Link to={"/setting"}>Setting</Link>
+              </li>
+              <li className="nav-item">
+                <a onClick={() => {}}>Sign In</a>
+              </li>
+              <li className="nav-item">
+                <a onClick={() => {}}>Sign Up</a>
+              </li>
+            </ul>
+          </div>
+          <Switch>
             <Route path="/" exact component={Newses} />
             <Route path="/words" component={Wordbook} />
             <Route path="/test" component={Test} />
             <Route path="/setting" component={Setting} />
-            <Route path="/signin" component={Signin} />
-            <Route path="/signup" component={Signup} />
+            {/* <Route path="/signin" component={Signin} />
+            <Route path="/signup" component={Signup} /> */}
             <Route path="/news/:id" component={News} />
             <Route
               exact
               path="/news"
               render={() => <h3>Please select a topic.</h3>}
             />
-          </nav>
-        </Router>
+          </Switch>
+        </nav>
       </div>
     );
   }
--- src/components/Newses.js
+++ src/components/Newses.js
@@ -2,52 +2,55 @@ import React, { Component } from "react";
 import ArticleList from "../mock-ups/ArticleList";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import Wordbook from "./Wordbook";
+import News from "./News";
+import Category from "./Category";
 
 export default class Newses extends Component {
   render() {
     return (
       <>
+        {/* <Category /> */}
         <div className="content">
-          <div className="news-list">
-            <div className="news-ul">
-              <Router>
-                {ArticleList.map(article => {
-                  let totalWord = article.ngram.reduce((a, b) => a + b);
-                  let ngramColor = ["red", "yellow", "green", "blue", "purple"];
-                  return (
-                    <Link key={article.id} to={`news/${article.id}`}>
-                      <div className="news-item">
-                        <div className="news-item-thumbnail-container">
-                          <img
-                            alt={`thumbnail${article.id}`}
-                            className="news-item-thumbnail"
-                            src={article.photoUrl}
-                          />
-                        </div>
-                        <div className="news-item-title">{article.title}</div>
-                        <div className="news-item-date">{article.date}</div>
-                        <div className="ngram-bar">
-                          {article.ngram.map((ngram, i) => {
-                            let percent = Math.round((ngram / totalWord) * 100);
-                            var style = {
-                              width: `${percent}%`,
-                              backgroundColor: ngramColor[i]
-                            };
-                            return (
-                              <div
-                                className="ngram-item"
-                                key={i}
-                                style={style}
-                              />
-                            );
-                          })}
-                        </div>
-                      </div>
-                    </Link>
-                  );
-                })}
-              </Router>
-            </div>
+          <div className="news-ul">
+            {ArticleList.map(article => {
+              let totalWord = article.ngram.reduce((a, b) => a + b);
+              let ngramColor = [
+                "rgb(206,74,74)",
+                "rgb(234,175,65)",
+                "rgb(72,165,106)",
+                "rgb(102,136,195)",
+                "rgb(178,93,166)"
+              ];
+              return (
+                <Link to={`news/${article.id}`}>
+                  <div className="news-item">
+                    <div className="news-item-thumbnail-container">
+                      <img
+                        alt={`thumbnail${article.id}`}
+                        className="news-item-thumbnail"
+                        src={article.photoUrl}
+                      />
+                    </div>
+
+                    <div className="news-item-title">{article.title}</div>
+
+                    <div className="news-item-date">{article.date}</div>
+                    <div className="ngram-bar">
+                      {article.ngram.map((ngram, i) => {
+                        let percent = Math.round((ngram / totalWord) * 100);
+                        var style = {
+                          width: `${percent}%`,
+                          backgroundColor: ngramColor[i]
+                        };
+                        return (
+                          <div className="ngram-item" key={i} style={style} />
+                        );
+                      })}
+                    </div>
+                  </div>
+                </Link>
+              );
+            })}
           </div>
         </div>
       </>
